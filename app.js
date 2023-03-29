e8b01ffc7488608ab2f9ef08646b412025ae3c67
Glen Hood <glenhoodatx@gmail.com>
2023-03-29 13:26:25 -0500

    Added Fajita choice button with parsed json by cat

--- app.js
+++ app.js
@@ -2,23 +2,31 @@
 $(function(){
 
     $.ajax({ 
-      type : 'GET', 
-      url : 'CateringItems.json', 
+      type : "GET", 
+      url : "CateringItems.json", 
       async : false, 
       beforeSend : function(){/*loading*/},
-      dataType : 'json', 
+      dataType : "json", 
       success : function(result){
-       var buffer="";
         $.each(result, function(index, val){ 
-    
-          for(var i=0; i < val.length; i++){ 
-            var item = val[i]; 
-            console.log(item.Title);
-            console.log(item.Price);
-            console.log(item.Category);
-            buffer+=" <li><a href='#"+item.Title+"'>"+item.Title+"</a></li> <li><a href='#"+item.Price+"'>"+item.Price+"</a></li> <li><a href='#"+item.Category+"'>"+item.Category+"</a></li>"; 
+        var items = ""
+          for(var i=0; i < val.length; i++){
+            
+            var item = val[i];
+            
+            if (val[i].Category === "Fajita Buffet") {
+                const title = val[i].Title;
+                // assuming that you have a <ul> element with an id of "fajita-titles" in your HTML
+                const ul = document.getElementById("fajita-titles");
+                const li = document.createElement("li");
+                li.appendChild(document.createTextNode(title));
+                ul.appendChild(li);
+              }
+            // console.log(item.Title);
+            // console.log(item.Price);
+            // console.log(item.Category);
+        
           } 
-          $('ul').html(buffer);
         });
       }
      });
