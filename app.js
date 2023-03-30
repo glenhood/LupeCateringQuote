e66b5bb540b5a062cf1aee357ba58eb235efa260
Glen Hood <glenhoodatx@gmail.com>
2023-03-30 17:30:46 -0500

    was able to populate price from parsed title

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 $(function(){
-
     $.ajax({ 
       type : "GET", 
       url : "CateringItems.json", 
@@ -9,38 +8,81 @@ $(function(){
       dataType : "json", 
       success : function(result){
         $.each(result, function(index, val){ 
-        var items = ""
+          var items = ""
           for(var i=0; i < val.length; i++){
             
             var item = val[i];
             
             if (val[i].Category === "Fajita Buffet") {
-                const title = val[i].Title;
-                const serves = val[i].Serves;
-                const sAmount = document.getElementById("sAmount");
-                const ul = document.getElementById("fajita-titles");
-                const li = document.createElement("li");
-                li.appendChild(document.createTextNode(title));
-                sAmount.appendChild(document.createTextNode(serves));
-                ul.appendChild(li);
-              }
-              
-            // console.log(item.Title);
-            // console.log(item.Price);
-            // console.log(item.Category);
-        
-          } 
+              const title = val[i].Title;
+              const ul = document.getElementById("fajita-titles");
+              const li = document.createElement("li");
+              li.appendChild(document.createTextNode(title));
+              ul.appendChild(li);
+            }
+          }
         });
       }
-     });
     });
-    $(document).ready(function() {
-        $('#fajita-titles').on('click', 'li', function() {
-            var value = $(this).text();
-            $('#output').val(value);
+  
+    $('#fajita-titles').on('click', 'li', function() {
+      var value = $(this).text();
+      $('#output').val(value);
+      var price = getPrice(value);
+      $('#price').val(price);
+      var price = getServes(value);
+      $('#serves').val(serves);
+    });
+  
+    function getPrice(title) {
+      var price = "";
+      $.ajax({ 
+        type : "GET", 
+        url : "CateringItems.json", 
+        async : false, 
+        dataType : "json", 
+        success : function(result){
+          $.each(result, function(index, val){ 
+            for(var i=0; i < val.length; i++){
+              var item = val[i];
+              if (item.Title === title) {
+                price = item.Price;
+                break;
+              }
+            }
+          });
+        }
+      });
+      return price;
+    }
+
+    function getServes(title) {
+        // var serves = "";
+        $.ajax({ 
+          type : "GET", 
+          url : "CateringItems.json", 
+          async : false, 
+          dataType : "json", 
+          success : function(result){
+            $.each(result, function(index, val){ 
+              for(var i=0; i < val.length; i++){
+                var item = val[i];
+                if (item.Title === title) {
+                  serves = item.Serves;
+                  break;
+                }
+              }
+            });
+          }
         });
-    });
+        return serves;
+      }
+});
+
+  
     
+    
+   
    
     var queryString = window.location.search.substring(1);
 
@@ -51,7 +93,6 @@ $(function(){
         params[pair[0]] = decodeURIComponent(pair[1] || '');
     });
     
-    // Populate the fields with the values
     document.getElementById('name').value = params['name'];
     document.getElementById('edate').value = params['edate'];
     document.getElementById('phone').value = params['phone'];
@@ -59,10 +100,18 @@ $(function(){
     document.getElementById('time').value = params['time'];
     document.getElementById('address').value = params['address'];
     document.getElementById('total').value = params['total'];
+    document.getElementById('second_total_field').value = params['total'];
     document.getElementById('location').value = params['location'];
     document.getElementById('theme').value = params['theme'];
     document.getElementById('lservice').value = params['lservice'];
-   
+
+    var total = parseFloat(document.getElementById('total').value);
+    var price = parseFloat($('#price').val());
+
+    if (!isNaN(total) && !isNaN(price)) {
+    var result = total * price;
+    document.getElementById('sum').value = result;
+    }
 
 
       
