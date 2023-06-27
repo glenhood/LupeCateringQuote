4ccb7737ddd87364cf1b82ad5da42b2c3173a426
Cosgrove <christopher.cosgrove@gmail.com>
2023-06-27 15:12:56 -0500

    email testing

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const sprequest = require("sp-request");
+const nodemailer = require("nodemailer")
+//setup connection for sprequest
 let spr = sprequest.create({
 	username: 'Lupe.Integration',
     password: 'Green0!',
     domain: 'RDMINC'
 });
-
+//setup smtp auth for nodemailer
+let transporter = nodemailer.createTransport(
+{
+	host:"lupetortilla-com.mail.protection.outlook.com",
+	port:25,
+	secure: false
+});
 
 const app = express();
 const port = process.env.PORT || 8600;
@@ -23,6 +31,10 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+//parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res) =>{
 	res.render('fieldsPage.html')
@@ -47,7 +59,7 @@ app.get("/CateringItemList.json", (req,res) => {
 			returnObject = {}
 			returnObject.Items = resultList
 			res.json(returnObject)
-			console.log(returnObject);
+			//console.log(returnObject);
 			
 			
 		})
@@ -59,6 +71,26 @@ app.get("/CateringItemList.json", (req,res) => {
 	;})
 
 
+app.post("/send" , (req,res) => {
+	// verify connection configuration
+	console.log(req.body.sender);
+
+
+	var mailOptions = {
+	  from: req.body.sender,
+	  to: req.body.email,
+	  subject: req.body.subject,
+	  html: req.body.emailBody
+	};
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+		console.log(error);
+	  } else {
+		console.log('Email sent: ' + info.response);
+	  }
+	});
+	
+})
 
 
 app.get("/api", (req, res) => {
